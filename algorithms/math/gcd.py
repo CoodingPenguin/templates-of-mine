2b309acb916fec015b1b3ee14a4e0818d7a6f02b
CoodingPenguin <unodostre@naver.com>
2020-12-30 14:13:55 +0900

    study: ✍ add gcd

---
+++ algorithms/math/gcd.py
@@ -0,0 +1,27 @@
+# title: 최대공약수
+# src: https://www.acmicpc.net/problem/2609
+# time: O(logN)
+
+def gcd_with_euclidean(x, y):
+    '''
+    유클리드 호제법으로 최대공약수 계산
+    - `x`, `y`: 최대공약수를 구하고자 하는 두 수
+    '''
+    if y == 0:
+        return x
+    return gcd_with_euclidean(y, x%y)
+
+
+def gcd_with_math_module(x, y):
+    '''
+    math.gcd()로 최대공약수 계산
+    - `x`, `y`: 최대공약수를 구하고자 하는 두 수   
+    '''
+    import math
+    return math.gcd(x, y)
+
+
+if __name__ == "__main__":
+    x, y = 10, 24
+    print(gcd_with_euclidean(x, y))
+    print(gcd_with_math_module(x, y))
